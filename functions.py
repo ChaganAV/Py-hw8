42802837c5d61e33334842605bee1574e3d629cf
Alexandr <jwchaganav@gmail.com>
2023-01-24 12:19:55 +0400

    3 уровень

--- functions.py
+++ functions.py
@@ -22,3 +22,33 @@ def FillList(text,digit,oper):
         else:
             oper.append(t)
 
+def EnumPrime(result,digit,oper):
+    digSimp = []
+    opSimp = []
+    for i in range(0,len(oper)):
+        if result==0:
+            if oper[i] == '*' or oper[i] == '/':
+                result = Calc(oper[i],digit[i],digit[i+1])
+                digSimp.append(result)
+            else:
+                digSimp.append(digit[i])
+                opSimp.append(oper[i])
+
+        else:
+            if oper[i] == '*' or oper[i] == '/':
+                result = Calc(oper[i],result,digit[i+1])
+                digSimp.append(result)
+            else:
+                opSimp.append(oper[i])
+                result = 0
+                if i==len(oper)-1:
+                    digSimp.append(digit[i+1])
+    result=0
+    result = Enum(result,digSimp,opSimp)
+    # for d in digSimp:
+    #     print(d)
+    # for o in opSimp:
+    #     print(o)
+    return result
+
+
--- py-hw8.py
+++ py-hw8.py
@@ -18,3 +18,13 @@ f.FillList(text2,digit,oper)
 res = 0
 res = f.Enum(res,digit,oper)
 print(f"{text2} = {res}")
+
+# Уровень 3
+# text3 = "12 - 4 * 2 + 6 / 3"
+text3 = "12 - 4 * 2 + 6 / 3 + 5"
+digit = []
+oper = []
+f.FillList(text3,digit,oper)
+res = 0
+res = f.EnumPrime(res,digit,oper)
+print(f"{text3} = {res}")
