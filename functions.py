5d4d2d8e0796893da928346f96be363204684ebd
Alexandr <jwchaganav@gmail.com>
2023-01-24 09:37:34 +0400

    перенес в функции обработку

--- functions.py
+++ functions.py
@@ -7,3 +7,18 @@ def Calc(oper,x,y):
         return x*y
     if oper=='/':
         return x/y
+def Enum(result,digit,oper):
+    for i in range(0,len(oper)):
+        if result==0:
+            result = Calc(oper[i],digit[i],digit[i+1])
+        else:
+            result = Calc(oper[i],res,digit[i+1])
+    return result
+
+def FillList(text,digit,oper):
+    for t in text.split(' '):
+        if t.isdigit():
+            digit.append(int(t))
+        else:
+            oper.append(t)
+
